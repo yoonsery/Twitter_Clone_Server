4afdfdf808cc5b6acd4e6c772d44002df6d8593c
sery <yunsel2@gmail.com>
2021-09-03 22:29:18 +0900

    dwitter make feature crud

--- app.js
+++ app.js
@@ -3,20 +3,13 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import 'express-async-errors';
-
 import tweetsRouter from './router/tweets.js';
-// import postRouter from './router/post.js';
-// import userRouter from './router/user.js';
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
-
-// app.use('/posts', postRouter);
-// app.use('/users', userRouter);
-
 app.use('/tweets', tweetsRouter);
 
 app.use((req, res, next) => {
--- router/tweets.js
+++ router/tweets.js
@@ -3,19 +3,19 @@ import 'express-async-errors';
 
 const router = express.Router();
 
-const tweets = [
+let tweets = [
   {
-    id: 1,
+    id: '1',
     text: 'Happy Halloween',
-    createAt: Date.now().toString(),
+    createdAt: Date.now().toString(),
     name: 'Hela',
     username: 'hela',
     url: 'https://randomuser.me/api/portraits/women/24.jpg',
   },
   {
-    id: 2,
+    id: '2',
     text: 'Merry X-mas',
-    createAt: Date.now().toString(),
+    createdAt: Date.now().toString(),
     name: 'sena',
     username: 'sena',
     url: 'https://randomuser.me/api/portraits/women/27.jpg',
@@ -27,13 +27,54 @@ const tweets = [
 router.get('/', (req, res, next) => {
   const username = req.query.username;
   const data = username
-    ? tweets.filter((t) => t.username === username)
+    ? tweets.filter((tweet) => tweet.username === username)
     : tweets;
   res.status(200).json(data);
 });
+
 // GET /tweets/:id
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
 // POST /tweets
+router.post('/', (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+
 // PUT /tweets/:id
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
 // DELETE /tweets/:id
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
 
 export default router;
