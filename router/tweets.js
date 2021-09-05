5d6cf0c6f1be2ce85fa6fc53a347b5feac5a5a64
sery <yunsel2@gmail.com>
2021-09-05 21:15:07 +0900

    add validator feature

--- router/tweets.js
+++ router/tweets.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+// validation
+const validateTweet = [
+  body('text')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('Text should be at least 3 characters'),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -12,10 +23,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweet);
 
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
