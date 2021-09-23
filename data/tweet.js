e5c09502dd87fd8f0ea92145546f232538fef74b
sery <yunsel2@gmail.com>
2021-09-23 22:42:57 +0900

    how to use mongoose at tweet.js

--- data/tweet.js
+++ data/tweet.js
@@ -1,59 +1,48 @@
-import MongoDb from 'mongodb';
-import { getTweets } from '../database/database.js';
+import Mongoose from 'mongoose';
+import { useVirtualId } from '../database/database.js';
 import * as userRepository from './auth.js';
 
+const tweetSchema = new Mongoose.Schema(
+  {
+    text: { type: String, require: true },
+    userId: { type: String, require: true },
+    name: { type: String, require: true },
+    username: { type: String, require: true },
+    url: String,
+  },
+  { timestamps: true }
+);
+
+useVirtualId(tweetSchema);
+const Tweet = Mongoose.model('Tweet', tweetSchema);
+
 export async function getAll() {
-  return getTweets().find().sort({ createdAt: -1 }).toArray().then(mapTweets);
+  return Tweet.find().sort({ createdAt: -1 });
 }
 
 export async function getAllByUsername(username) {
-  return getTweets()
-    .find({ username })
-    .sort({ createdAt: -1 })
-    .toArray()
-    .then(mapTweets);
+  return Tweet.find({ username }).sort({ createdAt: -1 });
 }
 
 export async function getById(id) {
-  return getTweets()
-    .findOne({ _id: new MongoDb.ObjectId(id) })
-    .then(mapOptionalTweet);
+  return Tweet.findById(id);
 }
 
 export async function create(text, userId) {
-  const { name, username, url } = await userRepository.findById(userId);
-  const tweet = {
+  const { name, username } = await userRepository.findById(userId);
+
+  return new Tweet({
     text,
-    createdAt: new Date(),
     userId,
     name,
     username,
-    url,
-  };
-  return getTweets()
-    .insertOne(tweet)
-    .then((data) => mapOptionalTweet({ ...tweet, _id: data.insertedId }));
+  }).save();
 }
 
 export async function update(id, text) {
-  return getTweets()
-    .findOneAndUpdate(
-      { _id: new MongoDb.ObjectId(id) },
-      { $set: { text } },
-      { returnDocument: 'after' } // 수정이 된 이후의 객체를 리턴해준다
-    )
-    .then((result) => result.value)
-    .then(mapOptionalTweet);
+  return Tweet.findByIdAndUpdate(id, { text }, { returnOriginal: false }); // returnOriginal을 false로 해야 업데이트한 값이 리턴이 된다
 }
 
 export async function remove(id) {
-  return getTweets().deleteOne({ _id: new MongoDb.ObjectId(id) });
-}
-
-function mapOptionalTweet(tweet) {
-  return tweet ? { ...tweet, id: tweet._id.toString() } : tweet;
-}
-
-function mapTweets(tweets) {
-  return tweets.map(mapOptionalTweet);
+  return Tweet.findOneAndDelete(id);
 }
