fdbaec3904b9ea6fa4b84640f638ce0575026b58
sery <yunsel2@gmail.com>
2021-09-23 18:16:00 +0900

    how to adapt mongoose at auth.js and database.js

--- data/auth.js
+++ data/auth.js
@@ -1,24 +1,25 @@
-import { getUsers } from '../database/database.js';
-import MongoDb from 'mongodb';
+import { useVirtualId } from '../database/database.js';
+import Mongoose from 'mongoose';
+
+const userSchema = new Mongoose.Schema({
+  username: { type: String, require: true },
+  name: { type: String, require: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+  url: { type: String, require: true },
+});
+
+useVirtualId(userSchema);
+const User = Mongoose.model('User', userSchema);
 
 export async function findByUsername(username) {
-  return getUsers()
-    .findOne({ username }) //
-    .then(mapOptionalUser);
+  return User.findOne({ username });
 }
 
 export async function findById(id) {
-  return getUsers()
-    .findOne({ _id: new MongoDb.ObjectId(id) })
-    .then(mapOptionalUser);
+  return User.findById(id);
 }
 
 export async function createUser(user) {
-  return getUsers()
-    .insertOne(user)
-    .then((data) => data.insertedId.toString());
-}
-
-function mapOptionalUser(user) {
-  return user ? { ...user, id: user._id.toString() } : user;
+  return new User(user).save().then((data) => data.id);
 }
--- database/database.js
+++ database/database.js
@@ -1,18 +1,21 @@
-import MongoDb from 'mongodb';
+import Mongoose from 'mongoose';
 import { config } from '../config.js';
 
-let db; // 여기서만 사용할 수 있는 변수 db를 만듦
-
 export async function connectDB() {
-  return MongoDb.MongoClient.connect(config.db.host) //
-    .then((client) => {
-      db = client.db();
-    });
+  return Mongoose.connect(config.db.host); //
 }
 
-export function getUsers() {
-  return db.collection('users');
+export function useVirtualId(schema) {
+  schema.virtual('id').get(function () {
+    return this._id.toString();
+  });
+
+  schema.set('toJSON', { virtuals: true });
+  schema.set('toObject', { virtuals: true });
 }
+
+// TODO(sery): Delete below
+let db;
 
 export function getTweets() {
   return db.collection('tweets');
