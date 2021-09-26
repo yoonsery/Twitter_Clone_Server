b8dac8e79074bf71e5d38958bdc57490d0b62c47
sery <yunsel2@gmail.com>
2021-09-26 23:34:16 +0900

    prep for heroku deployment

--- config.js
+++ config.js
@@ -25,6 +25,7 @@ export const config = {
     user: required('DB_USER'),
     database: required('DB_DATABASE'),
     password: required('DB_PASSWORD'),
+    port: required('DB_PORT'),
   },
   cors: {
     allowedOrigin: required('CORS_ALLOW_ORIGIN'),
--- db/database.js
+++ db/database.js
@@ -1,9 +1,16 @@
 import { config } from '../config.js';
 import SQ from 'sequelize';
 
-const { host, user, database, password } = config.db;
+const { host, port, user, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
-  dialect: 'mysql',
-  logging: false, // database 실행에 대한 로그가 콘솔에 남지 않게 하는 옵션
+  port,
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
